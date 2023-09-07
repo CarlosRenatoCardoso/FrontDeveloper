90a45d61d720db1d4c2bf1874ba15daec00b95c6
CarlosRenatoCardoso <127361048+CarlosRenatoCardoso@users.noreply.github.com>
2023-09-07 16:23:45 -0300

    :tada: Aula de 1 á 4

---
+++ js/login.js
@@ -0,0 +1,10 @@
+let email = document.getElementById('email');
+let senha = document.getElementById('senha');
+let btnEntrar = document.getElementById('btn-entrar');
+
+const emailBanco = "admin@admin";
+const senhaBanco = "123";
+
+btnEntrar.addEventListener('click',()=>{
+    
+});
