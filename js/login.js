c3658f38a4456b8a4b6eb770206f663cc964037f
CarlosRenatoCardoso <127361048+CarlosRenatoCardoso@users.noreply.github.com>
2023-09-16 17:36:47 -0300

    :sparkles: Função de Autenticação

--- js/login.js
+++ js/login.js
@@ -2,9 +2,51 @@ let email = document.getElementById('email');
 let senha = document.getElementById('senha');
 let btnEntrar = document.getElementById('btn-entrar');
 
-const emailBanco = "admin@admin";
-const senhaBanco = "123";
-
 btnEntrar.addEventListener('click',()=>{
-    window.open('cadastro-usuario.html','_self')
+    let userEmail = email.value;
+    let userSenha = senha.value;
+
+    if (!userEmail || !userSenha){
+        alert ("O campo de e-mail e senha são obrigatórios");
+    return;
+    }
+
+    autenticar(userEmail, userSenha);
 });
+
+function autenticar(email, senha){
+    const urlBase = `http://localhost:3400`;
+
+    fetch (`${urlBase}/login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({email, senha})
+    })
+    .then(response => response = response.json())
+    .then(response => {
+
+        if(!!response.mensagem){
+            alert(response.mensagem);
+            return;
+
+        }else{
+
+            alert("Usuario autenticado com sucesso!");
+
+            salvarToken(response.token);
+            salvarUsuario(response.usuario);
+
+            window.open('cadastro-usuario.html','_self')
+        }
+    });
+}
+
+function salvarToken(token){
+    localStorage.setItem('token', token)
+}
+
+function salvarUsuario(usuario){
+    localStorage.setItem('usuario', JSON.stringify(usuario))
+}
